7719aba0f7ef9c07d732b6955b7ae9d2348893d3
moshood <moshood.kareem@farmilik.com>
2025-09-07 09:39:17 +0100

    fix lint error

--- eslint.config.js
+++ eslint.config.js
@@ -4,31 +4,31 @@ import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
-  js.configs.recommended,
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        ecmaFeatures: { jsx: true },
-      },
-      env: {
-        es2021: true,
-        node: true,
-        browser: true,
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-      react: eslintPluginReact,
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-      "no-console": "off",
-      "no-undef": "off",
-    },
-  },
+	js.configs.recommended,
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "module",
+				ecmaFeatures: { jsx: true },
+			},
+		},
+		env: {
+			es2021: true,
+			node: true,
+			browser: true,
+		},
+		plugins: {
+			"@typescript-eslint": tsPlugin,
+			react: eslintPluginReact,
+		},
+		rules: {
+			"react/react-in-jsx-scope": "off",
+			"react/prop-types": "off",
+			"no-console": "off",
+			"no-undef": "off",
+		},
+	},
 ];
