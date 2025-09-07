2af7b5785afa77c024f3322d147f5460fe015c85
moshood <moshood.kareem@farmilik.com>
2025-09-07 13:27:40 +0100

    fixed prettiier

--- eslint.config.js
+++ eslint.config.js
@@ -5,36 +5,36 @@ import tsPlugin from "@typescript-eslint/eslint-plugin";
 import eslintPluginReact from "eslint-plugin-react";
 
 export default [
-  js.configs.recommended,
+	js.configs.recommended,
 
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        ecmaFeatures: { jsx: true },
-      },
-      globals: {
-        console: "readonly",
-        process: "readonly",
-        window: "readonly",
-        document: "readonly",
-        __dirname: "readonly",
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-      react: eslintPluginReact,
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-      "no-console": "off",
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      semi: ["error", "always"],
-      quotes: ["error", "double"],
-    },
-  },
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "module",
+				ecmaFeatures: { jsx: true },
+			},
+			globals: {
+				console: "readonly",
+				process: "readonly",
+				window: "readonly",
+				document: "readonly",
+				__dirname: "readonly",
+			},
+		},
+		plugins: {
+			"@typescript-eslint": tsPlugin,
+			react: eslintPluginReact,
+		},
+		rules: {
+			"react/react-in-jsx-scope": "off",
+			"react/prop-types": "off",
+			"no-console": "off",
+			"@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+			semi: ["error", "always"],
+			quotes: ["error", "double"],
+		},
+	},
 ];
