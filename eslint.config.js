7ba4e7793610363465b5cbae695ef12d5ca20075
moshood <moshood.kareem@farmilik.com>
2025-09-07 09:53:34 +0100

    tried fix suggestions

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,46 @@
+// eslint.config.js
 import js from "@eslint/js";
-import eslintPluginReact from "eslint-plugin-react";
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
+import eslintPluginReact from "eslint-plugin-react";
 
 export default [
-	js.configs.recommended,
-	{
-		files: ["**/*.ts", "**/*.tsx"],
-		languageOptions: {
-			parser: tsParser,
-			parserOptions: {
-				ecmaVersion: "latest",
-				sourceType: "module",
-				ecmaFeatures: { jsx: true },
-			},
-		},
-		globals: { ...globals.browser, ...globals.node },
-		plugins: {
-			"@typescript-eslint": tsPlugin,
-			react: eslintPluginReact,
-		},
-		rules: {
-			"react/react-in-jsx-scope": "off",
-			"react/prop-types": "off",
-			"no-console": "off",
-			"no-undef": "off",
-		},
-	},
+  js.configs.recommended,
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+      env: {
+        es2021: true,
+        node: true,
+        browser: true,
+      },
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        __dirname: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      react: eslintPluginReact,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+      "no-console": "off",
+      "no-undef": "off",
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+    },
+  },
 ];
