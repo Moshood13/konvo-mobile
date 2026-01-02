aa8c3c7d061b64c76619a79a27236ce855ea90b1
moshood <moshood.kareem@farmilik.com>
2026-01-02 22:02:29 +0100

    fix build error #3

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default [
+  {
+    ignores: [
+      'node_modules',
+      'dist',
+      'build',
+      '.expo',
+      'coverage'
+    ]
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended
+]
