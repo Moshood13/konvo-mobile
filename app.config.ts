a15cce3ccf4316fe501179935ac56370aec6b6d5
moshood <moshood.kareem@farmilik.com>
2026-01-03 16:33:22 +0100

    fix image path

--- app.config.ts
+++ app.config.ts
@@ -4,22 +4,22 @@ export default ({ config }) => {
 	const appEnv = process.env.APP_ENV || "development";
 
 	let appName = "Konvo (dev)";
-	let icon = "./assets/konvo-icon.png";
+	let icon = "./src/assets/konvo-icon.png";
 	let androidPackage = "com.konvo.dev";
 	let iosBundleId = "com.konvo.dev";
 
 	if (appEnv === "staging") {
 		appName = "konvo (staging)";
-		icon = "./assets/konvo-icon.png";
+		icon = "./src/assets/konvo-icon.png";
 		androidPackage = "com.konvo.staging";
 		iosBundleId = "com.konvo.staging";
 	} else if (appEnv === "production") {
 		appName = "Konvo";
-		icon = "./assets/konvo-icon.png";
+		icon = "./src/assets/konvo-icon.png";
 		androidPackage = "com.konvo";
 		iosBundleId = "com.konvo";
 	}
-
+	
 	return {
 		...config,
 		name: appName,
@@ -34,14 +34,14 @@ export default ({ config }) => {
 			fallbackToCacheTimeout: 0,
 		},
 		splash: {
-			image: "./assets/konvo-splashIcon.png",
+			image: "./src/assets/konvo-splashIcon.png",
 			resizeMode: "contain",
 			backgroundColor: "#ffffff",
 		},
 		android: {
 			package: androidPackage,
 			adaptiveIcon: {
-				foregroundImage: "./assets/konvo-icon.png",
+				foregroundImage: "./src/assets/konvo-icon.png",
 				backgroundColor: "#ffffff",
 			},
 		},
