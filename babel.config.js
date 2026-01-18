92ace8ed81ef44a17a589e95bc66251d07f49237
moshood <moshood.kareem@farmilik.com>
2026-01-18 22:45:52 +0100

    fix prettier formatting

--- babel.config.js
+++ babel.config.js
@@ -4,4 +4,4 @@ module.exports = function (api) {
 		presets: ["babel-preset-expo", "@babel/preset-typescript"],
 		plugins: ["react-native-paper/babel", "react-native-reanimated/plugin"],
 	};
-}
+};
